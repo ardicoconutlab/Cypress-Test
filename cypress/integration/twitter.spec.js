5665dc3b026efbae47b2b79550624917648eecad
Ardi Wirawan <ardi@coconutlab.io>
2020-06-08 08:41:02 +0700

    refactor: using cypress command for login & logout

--- cypress/integration/twitter.spec.js
+++ cypress/integration/twitter.spec.js
@@ -1,29 +1,13 @@
 describe("Retweet twitter tweet", () => {
   it("Success retweet first tweet", () => {
-    // login
     cy.visit("/");
-    cy.get("[data-testid=login]").click();
-    cy.url().should("eq", "https://twitter.com/login");
-    cy.get('input[name="session[username_or_email]"][type=text]')
-      .should("be.visible")
-      .type(Cypress.env("email"))
-      .should("have.value", Cypress.env("email"));
-    cy.get('input[name="session[password]"][type=password]')
-      .should("be.visible")
-      .type(Cypress.env("password"))
-      .should("have.value", Cypress.env("password"));
-    cy.get("[data-testid=LoginForm_Login_Button]")
-      .first()
-      .should("be.visible")
-      .click();
+    // login
+    cy.login();
     // open first tweet
     cy.get("[data-testid=tweet]", { timeout: 10000 }).first().click();
     cy.get("[data-testid=retweet]").first().click();
     cy.get("[data-testid=retweetConfirm]").should("be.visible").click();
     // logout
-    cy.get("[data-testid='SideNav_AccountSwitcher_Button']").click();
-    cy.get("[data-testid=AccountSwitcher_Logout_Button]").click();
-    cy.url().should("eq", "https://twitter.com/logout");
-    cy.get("[data-testid=confirmationSheetConfirm]").click();
+    cy.logout();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", () => {
+  cy.get("[data-testid=login]").click();
+  cy.url().should("eq", "https://twitter.com/login");
+  cy.get('input[name="session[username_or_email]"][type=text]')
+    .should("be.visible")
+    .type(Cypress.env("email"))
+    .should("have.value", Cypress.env("email"));
+  cy.get('input[name="session[password]"][type=password]')
+    .should("be.visible")
+    .type(Cypress.env("password"))
+    .should("have.value", Cypress.env("password"));
+  cy.get("[data-testid=LoginForm_Login_Button]")
+    .first()
+    .should("be.visible")
+    .click();
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.get("[data-testid='SideNav_AccountSwitcher_Button']").click();
+  cy.get("[data-testid=AccountSwitcher_Logout_Button]").click();
+  cy.url().should("eq", "https://twitter.com/logout");
+  cy.get("[data-testid=confirmationSheetConfirm]").click();
+});
